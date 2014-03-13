d822c24e7b656fd5ffa317e0bad3248e415b5c1c
purulentsoftware <purulentsoftware@gmail.com>
2014-03-13 15:23:44 -0400

    Initial Commit

---
+++ Time Card/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Time Card
+//
+//  Created by Chris Mays on 3/13/14.
+//  Copyright (c) 2014 Chris Mays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
