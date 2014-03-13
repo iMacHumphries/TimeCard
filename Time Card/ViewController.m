48da56d8fa736530ac86d2d6b82b20e99c422832
purulentsoftware <purulentsoftware@gmail.com>
2014-03-13 15:33:31 -0400

    Test

--- Time Card/ViewController.m
+++ Time Card/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad
 {
-    NSLog(@"First Commite");
+    NSLog(@"Hello");
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
