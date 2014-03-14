77c52d8db4bc29242e54489c67388386ae36e8b2
Benjamin Humphries <Ben@Benjamins-MacBook-Air.local>
2014-03-14 18:04:53 -0400

    MainMenuviewcontroller added

---
+++ Time Card/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Time Card
+//
+//  Created by Benjamin Humphries on 3/14/14.
+//  Copyright (c) 2014 Chris Mays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
