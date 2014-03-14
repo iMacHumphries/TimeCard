bc4e0da226de9c1452d4232634df78aafc37cda0
Benjamin Humphries <Ben@Benjamins-MacBook-Air.local>
2014-03-14 18:00:17 -0400

    CreateEmployeeViewcontroller

---
+++ Time Card/CreateEmployeeViewController.h
@@ -0,0 +1,15 @@
+//
+//  CreateEmployeeViewController.h
+//  Time Card
+//
+//  Created by Benjamin Humphries on 3/14/14.
+//  Copyright (c) 2014 Chris Mays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateEmployeeViewController : UIViewController{
+    
+}
+
+@end
---
+++ Time Card/CreateEmployeeViewController.m
@@ -0,0 +1,39 @@
+//
+//  CreateEmployeeViewController.m
+//  Time Card
+//
+//  Created by Benjamin Humphries on 3/14/14.
+//  Copyright (c) 2014 Chris Mays. All rights reserved.
+//
+
+#import "CreateEmployeeViewController.h"
+
+@interface CreateEmployeeViewController ()
+
+@end
+
+@implementation CreateEmployeeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
