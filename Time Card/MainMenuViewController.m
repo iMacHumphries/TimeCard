2b2c584fc04f514929372d46ad32904c138b57fe
purulentsoftware <purulentsoftware@gmail.com>
2014-03-14 20:04:43 -0400

    Added your changes

--- Time Card/MainMenuViewController.m
+++ Time Card/MainMenuViewController.m
@@ -142,6 +142,11 @@
     lastLoginLabel.text = [self getSatus];
    
     
+    NSString *clocked = [[clockInOutButton titleLabel]text];
+    
+    UIAlertView *alert =[[UIAlertView alloc]initWithTitle:[NSString stringWithFormat:@"Successfully %@",clocked] message:[NSString stringWithFormat:@"%@ was successfully %@",employee.name,clocked] delegate:self cancelButtonTitle:[self getRandomPraise] otherButtonTitles:nil, nil];
+    [alert show];
+    
   
     
 }
